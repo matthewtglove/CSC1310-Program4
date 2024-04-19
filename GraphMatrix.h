a9e38785ee2ccdd1a9ae60ead0cddaea2b1189dc
Matthew Love <matthewtglove@gmail.com>
2024-04-19 11:34:29 -0500

    Start implementation of the 2d array

--- GraphMatrix.h
+++ GraphMatrix.h
@@ -15,7 +15,43 @@ using namespace std;
 
 class GraphMatrix {
 private:
+    int **vertexMatrix;
+    int numVertices;
+    int numEdges;
+
 public:
+    GraphMatrix(int numVertices) {
+        this->numVertices = numVertices;
+        // TODO: Question for John, is this 2d array implemented correctly? I got this help from stackoverflow and ChatGPT on how to initialize a 2d array
+        vertexMatrix = new int *[numVertices];
+        for (int i = 0; i < numVertices; i++) {
+            // TODO: I'm not supposed to use a pointer here. Why?
+            vertexMatrix[i] = new int[numVertices];
+
+            // Then initialize everything to 0.
+            for (int j = 0; j < numVertices; j++) {
+                vertexMatrix[i][j] = 0;
+            }
+        }
+    }
+    ~GraphMatrix() {
+        // TODO: Is this correct?
+        for (int i = 0; i < numVertices; i++) {
+            delete[] vertexMatrix[i];
+        }
+        delete[] vertexMatrix;
+    }
+
+    void addEdge(int vertex1, int vertex2) {
+    }
+
+    void printGraph() {
+    }
+
+    bool isThereAnEdge(int vertex1, int vertex2) {
+        // If the value at that row and column is 1, then will return true. 0 will return false.
+        return (vertexMatrix[vertex1][vertex2]);
+    }
 };
 
 #endif
